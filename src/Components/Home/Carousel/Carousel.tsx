e2a3285fa97c0b1861b68a858e14bb7aa90a85cb
Pawel Piatek <pawelpiatek98@gmail.com>
2021-04-29 23:42:10 +0200

    added swiping logic to carousel

--- src/Components/Home/Carousel/Carousel.tsx
+++ src/Components/Home/Carousel/Carousel.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import {Row, Col} from 'antd';
 import {DoubleLeftOutlined, DoubleRightOutlined} from '@ant-design/icons';
 import {
@@ -9,21 +9,51 @@ import {
 } from './CarouselStyles';
 import office1 from '../../../assets/images/office1.jpg'
 import office2 from '../../../assets/images/office2.jpg'
+import office3 from '../../../assets/images/office3.jpg'
+import office4 from '../../../assets/images/office4.jpg'
 
 const Carousel: React.FC = () => {
-    return(
+    const images = [office1, office2, office3, office4];
+
+    const [activeImages, setActiveImages] = useState([0, 1]);
+
+    const swipeImagesLeft = () => {
+        setActiveImages(prevState => {
+            if (prevState[1] === images.length - 1) {
+                return [prevState[0] + 1, 0];
+            } else if (prevState[0] === images.length - 1) {
+                return [prevState[1], prevState[1] + 1];
+            } else {
+                return [prevState[0] + 1, prevState[1] + 1];
+            }
+        })
+    }
+
+    const swipeImagesRight = () => {
+        setActiveImages(prevState => {
+            if(prevState[0] === 0) {
+                return [images.length - 1, prevState[1] - 1];
+            } else if (prevState[1] === 0) {
+                return [prevState[0] - 1, prevState[0]];
+            } else {
+                return [prevState[0] - 1, prevState[1] - 1];
+            }
+        })
+    }
+
+    return (
         <CarouselWrapper>
             <Row gutter={[10, 0]}>
                 <Col xl={{span: 12}}>
-                    <CarouselItemLeft img={office1}>
-                        <CarouselItemButton mode='right'>
+                    <CarouselItemLeft img={images[activeImages[0]]}>
+                        <CarouselItemButton mode='right' onClick={() => swipeImagesRight()}>
                             <DoubleRightOutlined/>
                         </CarouselItemButton>
                     </CarouselItemLeft>
                 </Col>
                 <Col xl={{span: 12}}>
-                    <CarouselItemRight img={office2}>
-                        <CarouselItemButton mode='left'>
+                    <CarouselItemRight img={images[activeImages[1]]}>
+                        <CarouselItemButton mode='left' onClick={() => swipeImagesLeft()}>
                             <DoubleLeftOutlined/>
                         </CarouselItemButton>
                     </CarouselItemRight>
